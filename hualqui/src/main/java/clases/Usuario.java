115a0a15631a0e4988d720cbea7864d21760f8a9
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-11-22 20:33:11 -0300

    Añadido Usuario

---
+++ hualqui/src/main/java/clases/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author I.D
+ */
+public class Usuario {
+    String nombre;
+    String password;
+    String NombreUsu;
+    String Email;
+    int edad;
+}
