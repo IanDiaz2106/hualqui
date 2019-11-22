62670520504c2ee06fd248a0a7abb49c4aa81c41
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-11-22 20:11:02 -0300

    Multa y Apelacion class

---
+++ hualqui/src/main/java/clases/Multa.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author I.D
+ */
+public class Multa {
+    int Identificador;
+    String Calle;
+    String Tipo;
+    String Velocidad;
+    String Avenida;
+    String Pasaje;
+
+    public int getIdentificador() {
+        return Identificador;
+    }
+
+    public void setIdentificador(int Identificador) {
+        this.Identificador = Identificador;
+    }
+
+    public String getCalle() {
+        return Calle;
+    }
+
+    public void setCalle(String Calle) {
+        this.Calle = Calle;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public String getVelocidad() {
+        return Velocidad;
+    }
+
+    public void setVelocidad(String Velocidad) {
+        this.Velocidad = Velocidad;
+    }
+
+    public String getAvenida() {
+        return Avenida;
+    }
+
+    public void setAvenida(String Avenida) {
+        this.Avenida = Avenida;
+    }
+
+    public String getPasaje() {
+        return Pasaje;
+    }
+
+    public void setPasaje(String Pasaje) {
+        this.Pasaje = Pasaje;
+    }
+            
+    
+}
+
